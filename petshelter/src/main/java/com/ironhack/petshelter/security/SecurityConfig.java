b68e94e81f7a83fa88a149626f3d32c7706d2df1
pugeaale <pugeaale@gmail.com>
2024-10-27 22:15:09 +0100

    :coffin:

--- petshelter/src/main/java/com/ironhack/petshelter/security/SecurityConfig.java
+++ petshelter/src/main/java/com/ironhack/petshelter/security/SecurityConfig.java
@@ -70,8 +70,6 @@ public class SecurityConfig {
         // set up authorization for different request matchers and user roles
         http.authorizeHttpRequests((requests) -> requests
                 .requestMatchers("/api/login/**").permitAll()// public endpoint, we could add more if we wanted to
-                .requestMatchers("api/greet").permitAll()
-                .requestMatchers("api/greet/personal").hasAnyAuthority("ROLE_USER")
                 .requestMatchers(GET, "/api/users").hasAnyAuthority("ROLE_USER", "ROLE_ADMIN")
                 .requestMatchers(POST, "/api/users").hasAnyAuthority("ROLE_ADMIN")
                 .requestMatchers(POST, "/api/roles").hasAnyAuthority("ROLE_ADMIN")
