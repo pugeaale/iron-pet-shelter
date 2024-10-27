204dcf09415323ada8e0aa0183e562a9162c0d63
pugeaale <pugeaale@gmail.com>
2024-10-27 11:48:07 +0100

    :tada: set up project

---
+++ petshelter/src/test/java/com/ironhack/petshelter/PetshelterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ironhack.petshelter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetshelterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
