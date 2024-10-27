c3f1cd25a8d65381f0b7f68ed21cbcea29e195b9
pugeaale <pugeaale@gmail.com>
2024-10-27 20:43:26 +0100

    :sparkles: implementing dataloader

---
+++ petshelter/src/main/java/com/ironhack/petshelter/dataloader/DataLoader.java
@@ -0,0 +1,34 @@
+package com.ironhack.petshelter.dataloader;
+
+import com.ironhack.petshelter.model.Role;
+import com.ironhack.petshelter.model.User;
+import com.ironhack.petshelter.service.RoleService;
+import com.ironhack.petshelter.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class DataLoader implements CommandLineRunner {
+
+    private final UserService userService;
+    private final RoleService roleService;
+
+    @Override
+    public void run(String... args) throws Exception {
+        roleService.save(new Role("ROLE_USER"));
+        roleService.save(new Role("ROLE_ADMIN"));
+
+        userService.saveUser(new User("John Doe", "john", "1234"));
+        userService.saveUser(new User("James Smith", "james", "1234"));
+        userService.saveUser(new User("Jane Carry", "jane", "1234"));
+        userService.saveUser(new User("Chris Anderson", "chris", "1234"));
+
+        roleService.addRoleToUser("john", "ROLE_USER");
+        roleService.addRoleToUser("james", "ROLE_ADMIN");
+        roleService.addRoleToUser("jane", "ROLE_USER");
+        roleService.addRoleToUser("chris", "ROLE_ADMIN");
+        roleService.addRoleToUser("chris", "ROLE_USER");
+    }
+}
