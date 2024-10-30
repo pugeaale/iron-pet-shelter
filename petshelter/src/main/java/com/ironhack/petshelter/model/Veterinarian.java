bba197bcba44298b88bb6d9dcce007ef42e05c72
pugeaale <pugeaale@gmail.com>
2024-10-30 19:41:33 +0100

    :sparkles: implementing create Veterinarian use case (#11)

---
+++ petshelter/src/main/java/com/ironhack/petshelter/model/Veterinarian.java
@@ -0,0 +1,22 @@
+package com.ironhack.petshelter.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Veterinarian {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String firstName;
+
+    private String lastName;
+}
---
+++ petshelter/src/main/java/com/ironhack/petshelter/service/impl/VeterinarianServiceImpl.java
@@ -0,0 +1,24 @@
+package com.ironhack.petshelter.service.impl;
+
+import com.ironhack.petshelter.model.Veterinarian;
+import com.ironhack.petshelter.repository.VeterinarianRepository;
+import com.ironhack.petshelter.service.VeterinarianService;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class VeterinarianServiceImpl implements VeterinarianService {
+
+    private final VeterinarianRepository veterinarianRepository;
+
+    @Transactional
+    @Override
+    public Veterinarian save(Veterinarian veterinarian) {
+        log.info("Saving new veterinarian {} to the database", veterinarian.getLastName());
+        return veterinarianRepository.save(veterinarian);
+    }
+}
