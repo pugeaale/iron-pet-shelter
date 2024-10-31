9be30c96d29eee921787e3bb95cadc6cad7733f1
pugeaale <pugeaale@gmail.com>
2024-10-31 09:56:01 +0100

    :sparkles: implementing get Adopter by id (#14)

--- petshelter/src/main/java/com/ironhack/petshelter/service/AdopterService.java
+++ petshelter/src/main/java/com/ironhack/petshelter/service/AdopterService.java
@@ -10,4 +10,10 @@ public interface AdopterService {
      * @return the saved adopter
      */
     Adopter save(Adopter adopter);
+    /**
+     * This method is used to retrieve a adopter by id from the database.
+     * @param id the id of the adopter to be retrieved
+     * @return the retrieved adopter
+     */
+    Adopter getAdopterById(Integer id);
 }
