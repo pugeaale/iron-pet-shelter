bf40866846af4482b29478c11c286e095c372ec7
pugeaale <pugeaale@gmail.com>
2024-11-06 18:52:26 +0100

    :sparkles: implementing shelter validation on post (#44)

--- petshelter/src/main/java/com/ironhack/petshelter/model/Shelter.java
+++ petshelter/src/main/java/com/ironhack/petshelter/model/Shelter.java
@@ -2,6 +2,7 @@ package com.ironhack.petshelter.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -15,6 +16,7 @@ public class Shelter extends User {
 
     private String city;
 
+    @Pattern(regexp = "^\\+?[0-9 .\\-()]{7,15}$", message = "shelter phone number is invalid")
     private String phoneNumber;
 
     @OneToMany(mappedBy = "shelter")
