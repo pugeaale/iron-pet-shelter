e4646e5fd087cb0d2021a4081589955e812616d8
pugeaale <pugeaale@gmail.com>
2024-11-06 16:39:46 +0100

    :sparkles: implementing dog validation on post

--- petshelter/src/main/java/com/ironhack/petshelter/model/Dog.java
+++ petshelter/src/main/java/com/ironhack/petshelter/model/Dog.java
@@ -1,6 +1,7 @@
 package com.ironhack.petshelter.model;
 
 import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -11,10 +12,11 @@ public class Dog extends Animal {
 
     private Boolean okDog;
 
-    private  Boolean okCat;
+    private Boolean okCat;
 
     private Boolean okChild;
 
+    @NotEmpty(message = "dog breed must be not empty")
     private String breed;
 
     public Dog(String name) {
