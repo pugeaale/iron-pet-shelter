9074e53ba14c870bc31c7a9e09f50e7e5ee39648
pugeaale <pugeaale@gmail.com>
2024-11-07 09:18:54 +0100

    :sparkles: implementing access denied handler

--- petshelter/src/main/java/com/ironhack/petshelter/security/SecurityConfig.java
+++ petshelter/src/main/java/com/ironhack/petshelter/security/SecurityConfig.java
@@ -2,9 +2,9 @@ package com.ironhack.petshelter.security;
 
 import com.ironhack.petshelter.security.filters.CustomAuthenticationFilter;
 import com.ironhack.petshelter.security.filters.CustomAuthorizationFilter;
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -12,11 +12,12 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import static org.springframework.http.HttpMethod.*;
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
-
+import static org.springframework.http.HttpMethod.*;
 /**
  * This is the main configuration class for security in the application. It enables web security,
  * sets up the password encoder, and sets up the security filter chain.
@@ -31,7 +32,11 @@ public class SecurityConfig {
 
     // Autowired instance of the AuthenticationManagerBuilder (provided by Spring Security)
     private final AuthenticationManagerBuilder authManagerBuilder;
-
+    // Bean pour AccessDeniedHandler personnalisé
+    @Bean
+    public AccessDeniedHandler accessDeniedHandler() {
+        return new CustomAccessDeniedHandler();
+    }
 
     /**
      * Bean definition for AuthenticationManager
@@ -54,39 +59,38 @@ public class SecurityConfig {
      */
     @Bean
     protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        // CustomAuthenticationFilter instance created
+        // Instance du filtre d'authentification personnalisé
         CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authManagerBuilder.getOrBuild());
-
-        // set the URL that the filter should process
         customAuthenticationFilter.setFilterProcessesUrl("/api/login");
 
-        // disable CSRF protection because we are using tokens, not session
+        // Désactiver CSRF car on utilise des tokens au lieu de sessions
         http.csrf().disable();
 
-        // set the session creation policy to stateless, to not maintain sessions in the server
+        // Définir la politique de session en stateless
         http.sessionManagement().sessionCreationPolicy(STATELESS);
 
-        // set up authorization for different request matchers and user roles
+        // Définir les règles d'autorisation
         http.authorizeHttpRequests((requests) -> requests
-                .requestMatchers("/api/login/**").permitAll()// public endpoint, we could add more if we wanted to
+                .requestMatchers("/api/login/**").permitAll()
                 .requestMatchers(GET, "/api/users").hasAnyAuthority("ROLE_USER", "ROLE_ADMIN")
                 .requestMatchers(POST, "/api/users").hasAnyAuthority("ROLE_ADMIN")
                 .requestMatchers(POST, "/api/roles").hasAnyAuthority("ROLE_ADMIN")
                 .requestMatchers(POST, "/api/roles/add-to-user").hasAnyAuthority("ROLE_ADMIN")
-                .requestMatchers(DELETE, "/api//medical-procedures").hasAnyAuthority("ROLE_ADMIN")
+                .requestMatchers(DELETE, "/api/medical-procedures").hasAnyAuthority("ROLE_ADMIN")
                 .requestMatchers(POST, "/api/medical-procedures").hasAnyAuthority("ROLE_VETERINARIAN")
                 .requestMatchers(POST, "/api/adoptions").hasAnyAuthority("ROLE_ADOPTER")
                 .requestMatchers(POST, "/api/cats").hasAnyAuthority("ROLE_EMPLOYEE")
                 .requestMatchers(PATCH, "/api/shelters/add-employee").hasAnyAuthority("ROLE_SHELTER")
-                .anyRequest().authenticated()); // any other endpoints require authentication
+                .anyRequest().authenticated());
 
-        // add the custom authentication filter to the http security object
+        // Ajouter le filtre d'authentification personnalisé
         http.addFilter(customAuthenticationFilter);
 
-        // Add the custom authorization filter before the standard authentication filter.
+        // Ajouter le filtre d'autorisation personnalisé avant le filtre d'authentification standard
         http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
 
-        // Build the security filter chain to be returned.
+        http.exceptionHandling().accessDeniedHandler(accessDeniedHandler());
+
         return http.build();
     }
 }
