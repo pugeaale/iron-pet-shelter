ccea01095468b462ea1bf79a16d8311e0d4e560e
pugeaale <pugeaale@gmail.com>
2024-11-08 19:36:08 +0100

    :bug: fix conflict between global and validation handler exception (#64)

--- petshelter/src/main/java/com/ironhack/petshelter/exception/GlobalExceptionHandler.java
+++ petshelter/src/main/java/com/ironhack/petshelter/exception/GlobalExceptionHandler.java
@@ -2,11 +2,16 @@ package com.ironhack.petshelter.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * The type Global exception handler.
@@ -41,5 +46,17 @@ public class GlobalExceptionHandler {
     ErrorResponse errorDetails =
         new ErrorResponse(new Date(), HttpStatus.INTERNAL_SERVER_ERROR.toString() ,ex.getMessage(), request.getDescription(false));
     return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+  }
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+    Map<String, String> errors = new HashMap<>();
+    ex.getBindingResult().getAllErrors().forEach((error) -> {
+      String fieldName = ((FieldError) error).getField();
+      String errorMessage = error.getDefaultMessage();
+      errors.put(fieldName, errorMessage);
+    });
+    return ResponseEntity.badRequest().body(errors);
   }
 }
