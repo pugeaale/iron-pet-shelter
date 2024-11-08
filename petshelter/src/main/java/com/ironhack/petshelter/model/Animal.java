157ae83d510d2240c3b632198e241fa1ce04f6da
pugeaale <pugeaale@gmail.com>
2024-11-08 08:51:42 +0100

    :bug: fix infinite loop on get all adopters

--- petshelter/src/main/java/com/ironhack/petshelter/model/Animal.java
+++ petshelter/src/main/java/com/ironhack/petshelter/model/Animal.java
@@ -23,9 +23,11 @@ public class Animal {
 
     @ManyToOne
     @JoinColumn(name = "shelterId")
+    @JsonIgnore
     private Shelter shelter;
 
     @OneToOne(mappedBy = "animal")
+    @JsonIgnore
     private Adoption adoption;
 
     @OneToMany(mappedBy = "animal")
